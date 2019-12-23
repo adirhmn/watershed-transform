c30595b462cce7a55b27510781288e8f0d993e8a
adirhmn <adirahman47@gmail.com>
2019-12-23 12:45:27 +0700

    first commit

---
+++ Implementation code.py
@@ -0,0 +1,18 @@
+#Algorithm 4.6 Computing level components by breadth-first search using a fifo queue.
+
+def LevelComponents(V,E,im):
+    for p in D:
+        lab[p]=INIT
+    curlab=1
+    fifo_init(queue)
+
+    for (p in V) and (lab[p]==INIT):
+        lab[p]=curlab
+        fifo_add(p,queue)
+        while not fifo_empty(queue):
+            s=fifo_remove(queue)
+            for (q in NG) and (im[s]==im[q]):
+                if lab[q]==INIT:
+                    lab[q]=curlab
+                    fifo_add(q,queue)
+        curlab+=1
---
+++ readme.md
@@ -0,0 +1,2 @@
+Pada watershed transform saya megimplementasikan algoritma untuk 
+Komputasi komponen level dengan breadth-first search yang menggunakan antrian fifo.
